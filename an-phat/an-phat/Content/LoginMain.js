e063c5c48349dae4022daa14095c2730fff2b34b
satthuno999 <41618648+satthuno999@users.noreply.github.com>
2020-09-15 16:07:22 +0700

    login
    none

---
+++ an-phat/an-phat/Content/LoginMain.js
@@ -0,0 +1,55 @@
+﻿//LOGIN FB
+window.fbAsyncInit = function () {
+    FB.init({
+        appId: '3062992443809180',
+        cookie: true,
+        xfbml: true,
+        version: 'v8.0'
+    });
+
+    FB.AppEvents.logPageView();
+
+};
+
+(function (d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) { return; }
+    js = d.createElement(s); js.id = id;
+    js.src = "https://connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+
+function checkLoginState() {
+    FB.getLoginStatus(function (response) {
+        statusChangeCallback(response);
+    });
+}
+
+function statusChangeCallback(response) {  // Called with the results from FB.getLoginStatus().
+    console.log('statusChangeCallback');
+    console.log(response);                   // The current login status of the person.
+    if (response.status === 'connected') {   // Logged into your webpage and Facebook.
+        alert("Nguyên ăn cức " );
+
+    } else {                                 // Not logged into your webpage or we are unable to tell.
+        document.getElementById('status').innerHTML = 'Please log ' +
+            'into this webpage.';
+    }
+}
+
+//END LOGIN FB------------------------------------
+
+//Login Google====================
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+    alert('Email: ' + profile.getEmail());
+}
+
+    //button G
+
+    //End button
